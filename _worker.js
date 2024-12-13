68d8c007e6414ca622fd03dfdaeba9661ea0e38a
yasinghalandari <112236145+yasinghalandari@users.noreply.github.com>
2024-12-13 16:03:43 +0330

    worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '86530703-42e3-4c90-a0d4-3cc1cf5e55d7';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
